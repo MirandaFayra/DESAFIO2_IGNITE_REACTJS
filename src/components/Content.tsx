399870f67428f19e12607197210f23ee9a1b586e
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-03-06 13:08:47 -0300

    finalizar o componnete sidebar

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
+import { useEffect, useState } from 'react';
+import { GenreResponseProps,MovieProps } from '../@types/custom';
+import { MovieCard } from './MovieCard'
+import '../styles/content.scss'
+
+
+
 export function Content() {
-  // Complete aqui
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+  return(
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react"
+import { GenreResponseProps } from "../@types/custom"
+import { api } from "../services/api"
+import {Button} from './Button'
+import '../styles/sidebar.scss'
+
+
 export function SideBar() {
-  return (
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return(
     <nav className="sidebar">
         <span>Watch<p>Me</p></span>
 
@@ -16,6 +34,5 @@ export function SideBar() {
         </div>
 
       </nav>
-
   )
 }
