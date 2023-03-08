9113c89f359e2312bf786828ed43e35f9a189198
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-03-08 12:32:00 -0300

    Tirar a lógica principal dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import { useEffect, useState } from 'react';
-
 import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
-
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
-
 import { api } from './services/api';
-
 import './styles/global.scss';
-
 import './styles/sidebar.scss';
 import './styles/content.scss';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 interface GenreResponseProps {
   id: number;
@@ -30,70 +25,47 @@ interface MovieProps {
   Runtime: string;
 }
 
+
+
 export function App() {
+  /* Está sendo utilizado só na barra lateral */
   const [selectedGenreId, setSelectedGenreId] = useState(1);
 
+  /* Está sendo utilizado só na barra lateral */
   const [genres, setGenres] = useState<GenreResponseProps[]>([]);
 
+  /* Está sendo utilizado nos dois */
   const [movies, setMovies] = useState<MovieProps[]>([]);
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
 
+  /* Está sendo utilizado só na barra lateral */
   useEffect(() => {
     api.get<GenreResponseProps[]>('genres').then(response => {
       setGenres(response.data);
     });
-    console.log('aqui é 1 efeito')
   }, []);
 
   useEffect(() => {
     api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
       setMovies(response.data);
     });
-    console.log('aqui é 2 efeito')
 
     api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
       setSelectedGenre(response.data);
     })
-    console.log('aqui é 3 efeito')
   }, [selectedGenreId]);
 
+  /* Está sendo utilizado só na barra lateral */
   function handleClickButton(id: number) {
     setSelectedGenreId(id);
-    console.log('aqui é 4 efeito')
   }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
 
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
+      <SideBar/>
+      <Content/>
 
-      </nav>
-
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
     </div>
   )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,24 @@
 export function SideBar() {
   // Complete aqui
+  return(
+    <>
+    {/* Barra Lateral */}
+      <nav className="sidebar">
+          <span>Watch<p>Me</p></span>
+
+          <div className="buttons-container">
+            {genres.map(genre => (
+              <Button
+                key={String(genre.id)}
+                title={genre.title}
+                iconName={genre.name}
+                onClick={() => handleClickButton(genre.id)}
+                selected={selectedGenreId === genre.id}
+              />
+            ))}
+          </div>
+
+        </nav>
+    </>
+  )
 }
